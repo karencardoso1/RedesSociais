f13d11de39223fb25d3e3dadf7702dace78b0d33
karencardoso1 <151936876+karencardoso1@users.noreply.github.com>
2024-09-27 11:55:56 -0300

    paragrafo2.0

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -5,6 +5,6 @@ async function visualizarInformacoesGlobais() {
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container_texto')
     paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas`
-    console.log(dados);
+    console.log(paragrafo);
 }
 visualizarInformacoesGlobais()
